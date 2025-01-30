e67bd16014515d092bc6edc170805f23a8a74fc3
44Matt44 <matt123.3a@gmail.com>
2025-01-30 19:42:21 +0100

    Update base_shape.py

--- src/     shapes/base_shape.py
+++ src/     shapes/base_shape.py
@@ -1,33 +1,16 @@
+import math
 from abc import ABC, abstractmethod
-from typing import Tuple
 
-class GeometricShape(ABC):
-    def __init__(self, name: str):
-        self.name = name
-        self._validate_positive_values()
-        
-    @abstractmethod
-    def _validate_positive_values(self):
-        """Validate all shape parameters are positive"""
-        pass
-    
+class BaseShape(ABC):
     @abstractmethod
     def area(self) -> float:
         pass
-    
+
     @abstractmethod
     def perimeter(self) -> float:
         pass
-    
-    @property
-    def metadata(self) -> Tuple[str, dict]:
-        return (self.name, {
-            'area': self.area(),
-            'perimeter': self.perimeter()
 
-class BaseShape:
-    def area(self) -> float:
-        raise NotImplementedError("Subclasses must implement area()")
-    
-   def perimeter(self) -> float:  # Not implemented
-        raise NotImplementedError
+class Base3DShape(BaseShape):
+    @abstractmethod
+    def volume(self) -> float:
+        pass
