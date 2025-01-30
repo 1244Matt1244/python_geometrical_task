9dd0204e53c0a0ead474dae407f021a007bd4092
44Matt44 <matt123.3a@gmail.com>
2025-01-30 19:36:52 +0100

    Update test_sphere.py

--- src/     shapes/tests/test_sphere.py
+++ src/     shapes/tests/test_sphere.py
@@ -1,1 +1,8 @@
+# src/shapes/tests/test_sphere.py
+def test_sphere_volume():
+    sphere = Sphere(3)
+    assert sphere.volume() == (4/3) * math.pi * 3**3
 
+def test_negative_radius():
+    with pytest.raises(ValueError):
+        Sphere(-2)
