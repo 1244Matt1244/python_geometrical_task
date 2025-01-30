6b24c025332f94ba523d96a45a544eaffaf9ca51
44Matt44 <matt123.3a@gmail.com>
2025-01-30 17:40:36 +0100

    Update sphere.py

--- src/     shapes/sphere.py
+++ src/     shapes/sphere.py
@@ -1,1 +1,20 @@
+import math
+from ..base_shape import GeometricShape
 
+class Sphere(GeometricShape):
+    def __init__(self, radius: float):
+        self.radius = radius
+        super().__init__("Sphere")
+        
+    def _validate_positive_values(self):
+        if self.radius <= 0:
+            raise ValueError("Radius must be positive")
+            
+    def area(self) -> float:
+        return 4 * math.pi * self.radius ** 2
+        
+    def volume(self) -> float:
+        return (4/3) * math.pi * self.radius ** 3
+        
+    def perimeter(self) -> float:
+        raise NotImplementedError("Sphere does not have perimeter")
