ebd62e22783f3c3356e37c059b83491b54246509
44Matt44 <matt123.3a@gmail.com>
2025-01-30 17:40:55 +0100

    Update base_shape.py

--- src/     shapes/base_shape.py
+++ src/     shapes/base_shape.py
@@ -1,1 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
 
+class GeometricShape(ABC):
+    def __init__(self, name: str):
+        self.name = name
+        self._validate_positive_values()
+        
+    @abstractmethod
+    def _validate_positive_values(self):
+        """Validate all shape parameters are positive"""
+        pass
+    
+    @abstractmethod
+    def area(self) -> float:
+        pass
+    
+    @abstractmethod
+    def perimeter(self) -> float:
+        pass
+    
+    @property
+    def metadata(self) -> Tuple[str, dict]:
+        return (self.name, {
+            'area': self.area(),
+            'perimeter': self.perimeter()
+        })
