c9faeaf576c505194c5805fe73732c16732224ef
44Matt44 <matt123.3a@gmail.com>
2025-01-31 00:52:24 +0100

    Update base_shape.py

--- src/     shapes/base_shape.py
+++ src/     shapes/base_shape.py
@@ -1,1 +1,20 @@
+from abc import ABC, abstractmethod
+import math
 
+class BaseShape2D(ABC):
+    @abstractmethod
+    def area(self) -> float:
+        pass
+    
+    @abstractmethod
+    def perimeter(self) -> float:
+        pass
+
+class BaseShape3D(ABC):
+    @abstractmethod
+    def surface_area(self) -> float:
+        pass
+    
+    @abstractmethod
+    def volume(self) -> float:
+        pass
