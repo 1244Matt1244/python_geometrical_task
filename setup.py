fa07b205a39305ae921c3990f7630ea6d6fd0466
44Matt44 <matt123.3a@gmail.com>
2025-01-30 23:24:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="geometric_shapes",
     version="0.3",
     author="Your Name",
     author_email="your.email@example.com",
     description="Advanced geometric shape calculations",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
@@ -29,10 +24,5 @@ setup(
     entry_points={
         "console_scripts": ["geoshapes=shapes.cli:main"]
     },
-    python_requires=">=3.8",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    python_requires=">=3.8"
 )
