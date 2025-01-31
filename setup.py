e6ae69096720497456a7939769cbda31cfd86582
44Matt44 <matt123.3a@gmail.com>
2025-01-31 01:21:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="geometric-shapes",
-    version="1.0",
+    version="1.0.2",
+    description="Python package for calculating geometric properties",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[
-        "click>=8.1.0",
-    ],
-    extras_require={
-        "test": ["pytest>=7.0.0"],
-    },
-    entry_points={
-        "console_scripts": [
-            "geoshapes=shapes.cli:main"
-        ]
-    },
+    install_requires=["click>=8.1.0"],
+    extras_require={"test": ["pytest>=7.0.0"]},
+    entry_points={"console_scripts": ["geoshapes=shapes.cli:main"]},
     python_requires=">=3.8",
 )
