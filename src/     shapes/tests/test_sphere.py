22e0c9af6e93db0ec0b4839f0a6902d141d29969
44Matt44 <matt123.3a@gmail.com>
2025-01-31 01:00:54 +0100

    Update test_sphere.py

--- src/     shapes/tests/test_sphere.py
+++ src/     shapes/tests/test_sphere.py
@@ -1,13 +1,8 @@
 import pytest
-import math
-from src.shapes.sphere import Sphere
+from shapes.sphere import Sphere
 
+def test_sphere_surface_area():
+    assert pytest.approx(Sphere(3).surface_area(), rel=1e-3) == 113.097
 
 def test_sphere_volume():
-    sphere = Sphere(3)
-    assert sphere.volume() == (4/3) * math.pi * 3**3
-
-
-def test_negative_radius():
-    with pytest.raises(ValueError):
-        Sphere(-2)
+    assert pytest.approx(Sphere(3).volume(), rel=1e-3) == 113.097
