8f3eba223d40169d20b59f261ad69bf67309f776
44Matt44 <matt123.3a@gmail.com>
2025-02-01 12:56:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@ setup(
     ],
     extras_require={
         "dev": [
-            "pytest",
-            "pre-commit",
-            "mypy",
-            "sphinx"
+            "pytest>=7.2",
+            "pytest-cov>=4.0",
+            "flake8>=6.0",
+            "mypy>=1.0",
+            "sphinx>=7.0"  # Add documentation tool
         ]
     },
     python_requires=">=3.8, <3.13"  # Adjust this to the minimum and maximum Python version you want to support
